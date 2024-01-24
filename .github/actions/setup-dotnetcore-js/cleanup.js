84f22fbefe2c8022a751928a31936c28b1a37abe
Agastya Kommanamanchi <kommanamanchi.agastya@gmail.com>
2024-01-23 21:40:57 -0500

    modified workflow

--- .github/actions/setup-dotnetcore-js/cleanup.js
+++ .github/actions/setup-dotnetcore-js/cleanup.js
@@ -2,20 +2,30 @@ const fs = require('fs').promises;
 const path = require('path');
 
 async function deleteFolder(folderPath) {
+  console.log(`Accessing directory: ${folderPath}`);
   const entries = await fs.readdir(folderPath, { withFileTypes: true });
   for (const entry of entries) {
     const fullPath = path.join(folderPath, entry.name);
     if (entry.isDirectory()) {
+      console.log(`Deleting subdirectory: ${fullPath}`);
       await deleteFolder(fullPath);
     } else {
+      console.log(`Deleting file: ${fullPath}`);
       await fs.unlink(fullPath);
     }
   }
+  console.log(`Removing directory: ${folderPath}`);
   await fs.rmdir(folderPath);
 }
 
-const downloadFolderPath = path.join(process.env['GITHUB_WORKSPACE'], 'dotnet');
-deleteFolder(downloadFolderPath).catch(err => {
-  console.error(err);
+const dotnetPath = path.join(process.env['GITHUB_WORKSPACE'], 'dotnet');
+console.log('Starting cleanup process...');
+
+deleteFolder(dotnetPath).catch(err => {
+  console.error(`Error during cleanup: ${err}`);
   process.exit(1);
 });
+
+console.log(`Successfully deleted: ${dotnetPath}`);
+
+console.log('Finished Cleanup Process...');
